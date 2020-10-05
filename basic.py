89d003d2811d138bb00b84accf26aa4e03eaca35
yalinanaliz <kuyalin@gmail.com>
2020-10-05 17:04:43 +0300

    line color, marker type, widht changed

--- basic.py
+++ basic.py
@@ -6,6 +6,21 @@ np.random.seed(42)
 random_x = np.random.randint(1,101,100)
 random_y = np.random.randint(1,101,100)
 
-data = [go.Scatter(x=random_x, y=random_y,mode='markers')]
+data = [go.Scatter(x=random_x,
+             y=random_y,
+             mode='markers',
+             marker=dict(
+                 size=12,
+                 color='rgb(51,204,153)',
+                 symbol='pentagon',
+                 line={'width':2}
+             ))]
+             
 
-pyo.plot(data)
+layout = go.Layout(title='Hello First Plot',
+                    xaxis={'title':'My x axis'},
+                    yaxis = dict(title='My y axis'),
+                    hovermode='closest')
+
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='scatter.html')
