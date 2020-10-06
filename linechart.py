89fc796b0e196b9be410328d8bc2a96892899c33
yalinanaliz <kuyalin@gmail.com>
2020-10-06 15:57:20 +0300

    interactive line chart

--- linechart.py
+++ linechart.py
@@ -1,6 +1,7 @@
 import numpy as np
 import plotly.offline as pyo
 import plotly.graph_objs as go 
+
 np.random.seed(56)
 
 x_values = np.linspace(0,1,100)
@@ -9,16 +10,16 @@ y_values = np.random.randn(100)
 trace0 = go.Scatter(x=x_values, y=y_values+5,
                   mode='markers', name='markers')
 
-trace1 = go.Scatter(x=x_values, y=y_values,
+trace1 = go.Scatter(x=x_values, y=y_values+5,
                   mode='lines', name='mylines')
 
-trace2 = go.Scatter(x=x_values, y=y_values-5,
-                  mode='lines+markers', name='my favorite')                  
+#trace2 = go.Scatter(x=x_values, y=y_values-5,
+                  #mode='lines+markers', name='my favorite')                  
 
     
-data = [trace0, trace1, trace2]
+data = [trace0, trace1]
 
 layout = go.Layout(title='Line Charts')
 
 fig = go.Figure(data=data, layout=layout)
-pyo.plot(fig)
+pyo.plot(fig, filename='linechart.html')
